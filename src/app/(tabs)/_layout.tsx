2d6926fd1a7f3c1be4c89e077f32af3e044b0691
Adnan <adbak.dev@gmail.com>
2024-09-22 20:48:07 +0200

    WIP

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -3,69 +3,73 @@ import { BlurView } from 'expo-blur'
 import { Tabs } from 'expo-router'
 import { StyleSheet, Text } from 'react-native'
 import { FontAwesome, FontAwesome6, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'
+import { FloatingPlayer } from '@/components/FloatingPlayer'
 
 const TabsNavigation = () => {
 	return (
-		<Tabs
-			screenOptions={{
-				tabBarActiveTintColor: colors.primary,
-				tabBarLabelStyle: {
-					fontSize: fontSize.xs,
-					fontWeight: 500,
-				},
-				headerShown: false,
-				tabBarStyle: {
-					position: 'absolute',
-					borderTopLeftRadius: 20,
-					borderTopRightRadius: 20,
-					borderTopWidth: 0,
-				},
-				tabBarBackground: () => (
-					<BlurView
-						intensity={95}
-						style={{
-							...StyleSheet.absoluteFillObject,
-							borderTopLeftRadius: 20,
-							borderTopRightRadius: 20,
-							overflow: 'hidden',
-						}}
-					/>
-				),
-			}}
-		>
-			<Tabs.Screen
-				name="favorites"
-				options={{
-					title: 'Favorites',
-					tabBarIcon: () => <FontAwesome name="heart" size={20} color={colors.primary} />,
-				}}
-			/>
-			<Tabs.Screen
-				name="playlists"
-				options={{
-					title: 'Playlists',
-					tabBarIcon: () => (
-						<MaterialCommunityIcons name="playlist-play" size={28} color={colors.primary} />
+		<>
+			<Tabs
+				screenOptions={{
+					tabBarActiveTintColor: colors.primary,
+					tabBarLabelStyle: {
+						fontSize: fontSize.xs,
+						fontWeight: 500,
+					},
+					headerShown: false,
+					tabBarStyle: {
+						position: 'absolute',
+						borderTopLeftRadius: 20,
+						borderTopRightRadius: 20,
+						borderTopWidth: 0,
+					},
+					tabBarBackground: () => (
+						<BlurView
+							intensity={95}
+							style={{
+								...StyleSheet.absoluteFillObject,
+								borderTopLeftRadius: 20,
+								borderTopRightRadius: 20,
+								overflow: 'hidden',
+							}}
+						/>
 					),
 				}}
-			/>
-			<Tabs.Screen
-				name="(songs)"
-				options={{
-					title: 'Songs',
-					tabBarIcon: () => (
-						<Ionicons name="musical-notes-sharp" size={24} color={colors.primary} />
-					),
-				}}
-			/>
-			<Tabs.Screen
-				name="artists"
-				options={{
-					title: 'Artists',
-					tabBarIcon: () => <FontAwesome6 name="users-line" size={20} color={colors.primary} />,
-				}}
-			/>
-		</Tabs>
+			>
+				<Tabs.Screen
+					name="favorites"
+					options={{
+						title: 'Favorites',
+						tabBarIcon: () => <FontAwesome name="heart" size={20} color={colors.primary} />,
+					}}
+				/>
+				<Tabs.Screen
+					name="playlists"
+					options={{
+						title: 'Playlists',
+						tabBarIcon: () => (
+							<MaterialCommunityIcons name="playlist-play" size={28} color={colors.primary} />
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="(songs)"
+					options={{
+						title: 'Songs',
+						tabBarIcon: () => (
+							<Ionicons name="musical-notes-sharp" size={24} color={colors.primary} />
+						),
+					}}
+				/>
+				<Tabs.Screen
+					name="artists"
+					options={{
+						title: 'Artists',
+						tabBarIcon: () => <FontAwesome6 name="users-line" size={20} color={colors.primary} />,
+					}}
+				/>
+			</Tabs>
+			<FloatingPlayer />
+		</>
 	)
 }
 
--- src/components/TracksList.tsx
+++ src/components/TracksList.tsx
@@ -1,27 +1,58 @@
-import { FlatList, FlatListProps, View } from 'react-native'
-import { TracksListItem } from './TrackListItem'
-import library from '@/assets/data/library.json'
-import { utilsStyles } from '@/styles'
-import { Track } from 'react-native-track-player'
+import { TracksListItem } from '@/components/TrackListItem'
+import { unknownTrackImageUri } from '@/constants/images'
 
-export type TrackListProps = Partial<FlatListProps<Track>> & {
+import { utilsStyles } from '@/styles'
+import { useRef } from 'react'
+import { FlatList, FlatListProps, Text, View } from 'react-native'
+import FastImage from 'react-native-fast-image'
+import TrackPlayer, { Track } from 'react-native-track-player'
+import { QueueControls } from './QueuControls'
+
+export type TracksListProps = Partial<FlatListProps<Track>> & {
+	id: string
 	tracks: Track[]
+	hideQueueControls?: boolean
 }
 
-const itemDivider = () => (
+const ItemDivider = () => (
 	<View style={{ ...utilsStyles.itemSeparator, marginVertical: 9, marginLeft: 60 }} />
 )
 
-export const TrackList = ({ tracks, ...flatlistProps }: TrackListProps) => {
-	const handleTrackSelect = (track: Track) => {
-		console.log('Selected track:', track)
+export const TracksList = ({
+	id,
+	tracks,
+	hideQueueControls = false,
+	...flatlistProps
+}: TracksListProps) => {
+	const queueOffset = useRef(0)
+
+	const handleTrackSelect = async (selectedTrack: Track) => {
+		const trackIndex = tracks.findIndex((track) => track.url === selectedTrack.url)
+
+		if (trackIndex === -1) return
 	}
+
 	return (
 		<FlatList
 			data={tracks}
-			contentContainerStyle={{ padding: 10, paddingBottom: 128 }}
-			ItemSeparatorComponent={itemDivider}
-			ListFooterComponent={itemDivider}
+			contentContainerStyle={{ paddingTop: 10, paddingBottom: 128 }}
+			ListHeaderComponent={
+				!hideQueueControls ? (
+					<QueueControls tracks={tracks} style={{ paddingBottom: 20 }} />
+				) : undefined
+			}
+			ListFooterComponent={ItemDivider}
+			ItemSeparatorComponent={ItemDivider}
+			ListEmptyComponent={
+				<View>
+					<Text style={utilsStyles.emptyContentText}>No songs found</Text>
+
+					<FastImage
+						source={{ uri: unknownTrackImageUri, priority: FastImage.priority.normal }}
+						style={utilsStyles.emptyContentImage}
+					/>
+				</View>
+			}
 			renderItem={({ item: track }) => (
 				<TracksListItem track={track} onTrackSelect={handleTrackSelect} />
 			)}
